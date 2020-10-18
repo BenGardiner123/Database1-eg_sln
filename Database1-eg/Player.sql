de1fd798c59d82865ef4760de4fac4f25e3da8cc
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-10-19 08:41:33 +1100

    Add project files.

---
+++ Database1-eg/Player.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Player]
+(
+	[Username] NVARCHAR(50) NOT NULL PRIMARY KEY
+)
---
+++ Database1-eg/Turn.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Turn]
+(
+	[Username] nvarchar(50) NOT NULL, 
+	[TurnID] datetime NOT NULL, 
+	[playerChoice] nvarchar(12) NOT NULL,
+	[aiChoice] nvarchar(50) NOT NULL, 
+	primary key (Username, TurnID), 
+	foreign key (Username) references Player
+)
