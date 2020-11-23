5379b843e815649b90b5fa34d8267751bccfab9e
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-11-23 11:05:45 +1100

    adding the view

---
+++ Database1-eg/Leaderboard.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW Leaderboard as 
+
+SELECT played.Username as Player, (cast(wins.numwins as float)/cast(played.numplayed as float)) AS Winratio, played.numplayed as Turnsplayed
+FROM 
+(	select username, count(*) as numwins
+	from Turn
+	where
+	(p1Choice = 'Rock' and p2Choice = 'Scissors'
+	 or p1Choice = 'Scissors' and p2Choice = 'Paper'
+	 or p1Choice = 'Paper' and p2Choice = 'Rock')
+	 group by username 
+	) wins
+INNER JOIN
+(	select username, count(*) as numplayed
+	from Turn
+	group by username
+) played 
+on wins.username = played.username
+go
